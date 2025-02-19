aeb12151c14bb6484f3b6f3f55fd98b2a62f8576
Yoong Jun Quan <yoongjunquan@gmail.com>
2025-02-19 19:58:52 +0800

    feat(api): prettier format file

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -2,11 +2,11 @@ import { configDefaults, defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [tsconfigPaths()],
-    test: {
-        exclude: [...configDefaults.exclude],
-        coverage: {
-            reporter: ['text', 'html', 'clover', 'json'],
-        },
+  plugins: [tsconfigPaths()],
+  test: {
+    exclude: [...configDefaults.exclude],
+    coverage: {
+      reporter: ['text', 'html', 'clover', 'json'],
     },
+  },
 });
